89ff07aa2fb68035c9cd5158a1dece07de2e0471
Frédéric Melaerts <Frederic.Melaerts@student.uantwerpen.be>
2017-04-19 17:37:06 +0200

    Thyleaf ready

--- src/main/java/be/uantwerpen/Controller/cardController.java
+++ src/main/java/be/uantwerpen/Controller/cardController.java
@@ -1,8 +1,10 @@
 package be.uantwerpen.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -12,9 +14,21 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class cardController {
-    @RequestMapping({"/"})
+    @RequestMapping({"/test"})
     public String showHomepage(final ModelMap model)
-            {
-                return "reception";
-            }
+    {
+        return "reception";
+    }
+
+    @RequestMapping({"/hello"})
+    public String showHello(final ModelMap model)
+    {
+        return "testPage";
+    }
+    @RequestMapping({"/lol"})
+    public String lol(final ModelMap model)
+    {
+        return "lol";
+    }
+
 }
