d443af6abcf3abba994399d6a10b9a8d794c985e
SirBlackser <dries.blontrock@student.uantwerpen.be>
2017-04-19 15:30:52 +0200

    Test

---
+++ src/main/java/be/uantwerpen/SupahuberApplication.java
@@ -0,0 +1,12 @@
+package be.uantwerpen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupahuberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupahuberApplication.class, args);
+	}
+}
