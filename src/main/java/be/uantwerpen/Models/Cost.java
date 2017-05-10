a404c99004234d5032cffbeab98bff0796fb5830
SirBlackser <dries.blontrock@student.uantwerpen.be>
2017-05-10 17:44:57 +0200

    begin van graphbuilder

---
+++ src/main/java/be/uantwerpen/Models/Cost.java
@@ -0,0 +1,43 @@
+package be.uantwerpen.Models;
+
+/**
+ * Created by dries on 10/05/2017.
+ */
+public class Cost {
+    private boolean status;
+    private long weightToStart;
+    private long weight;
+    private long idVehicle;
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public long getWeightToStart() {
+        return weightToStart;
+    }
+
+    public void setWeightToStart(long weightToStart) {
+        this.weightToStart = weightToStart;
+    }
+
+    public long getWeight() {
+        return weight;
+    }
+
+    public void setWeight(long weight) {
+        this.weight = weight;
+    }
+
+    public long getIdVehicle() {
+        return idVehicle;
+    }
+
+    public void setIdVehicle(long idVehicle) {
+        this.idVehicle = idVehicle;
+    }
+}
