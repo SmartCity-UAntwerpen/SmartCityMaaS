fb034a515e6dc94591e18f6e601b90c569b6f9e1
SirBlackser <dries.blontrock@student.uantwerpen.be>
2017-06-07 11:02:08 +0200

    Removed databaseLoader

--- src/main/java/be/uantwerpen/data/DatabaseLoader.java
+++ src/main/java/be/uantwerpen/data/DatabaseLoader.java
@@ -37,7 +37,7 @@ public class DatabaseLoader {
 
     @PostConstruct
     private void initDatabase() {
-        Permission p1 = new Permission("logon");
+        /*Permission p1 = new Permission("logon");
         permissionRepository.save(p1);
         Permission p2 = new Permission("secret-message");
         permissionRepository.save(p2);
@@ -64,7 +64,7 @@ public class DatabaseLoader {
         roles = new ArrayList<>();
         roles.add(User);
         u2.setRoles(roles);
-        userRepository.save(u2);
+        userRepository.save(u2);*/
 
         /*Delivery delivery = new Delivery("Lorance", "Malfait","Seg 9", "Seg 10",2);
         deliveryRepository.save(delivery);
