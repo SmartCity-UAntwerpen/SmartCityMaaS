ce08242a0d376b6501fadff924de591b96e53169
Raf Van den Langenbergh <raf.vandenlangenbergh@student.uantwerpen.be>
2018-12-18 10:33:42 +0100

    Possible fix Mongo

--- src/main/java/be/uantwerpen/SmartcityMaaS.java
+++ src/main/java/be/uantwerpen/SmartcityMaaS.java
@@ -18,9 +18,6 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import java.util.Locale;
 
 @SpringBootApplication
-/*
-@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})*/
-@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class SmartcityMaaS extends WebMvcConfigurerAdapter {
 
 	private static final Logger logger = LogManager.getLogger(SmartcityMaaS.class);
