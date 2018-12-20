c7b61f44cf6ffbc0ebd828759d67e48fb372ac7b
Raf Van den Langenbergh <raf.vandenlangenbergh@student.uantwerpen.be>
2018-12-20 16:16:43 +0100

    Removed unused imports from tests Added MaaS banner

--- src/test/java/be/uantwerpen/controller/DeliveryControllerTest.java
+++ src/test/java/be/uantwerpen/controller/DeliveryControllerTest.java
@@ -12,14 +12,12 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @RunWith(SpringRunner.class)
